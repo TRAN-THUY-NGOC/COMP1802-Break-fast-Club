31a150e5b3d99ae410fcc72779581528632ba017
TRAN-THUY-NGOC <Ngoctran1816@gmail.com>
2023-11-21 22:50:00 +0700

    init project

---
+++ assets/style/changebg.js
@@ -0,0 +1,25 @@
+var bg = [
+  './assets/images/bg1.png',
+  './assets/images/bg5.jpg',
+];
+var audioSources = [
+      './assets/Music/Nhạc tết.mp3',
+      './assets/Music/Babysharkdododo.mp3',
+    ];
+      document.querySelector("#bg1").addEventListener("click", function () {
+      document.querySelector("#background_image img").src = bg[0];
+});
+      document.querySelector("#bg2").addEventListener("click", function () {
+      document.querySelector("#background_image img").src = bg[1];
+});
+      document.querySelector("#nhac1").addEventListener("click", function () {
+      document.querySelector("#audio source").src = audioSources[0];
+      document.querySelector("#audio").load();
+      document.querySelector("#audio").play();
+});
+      document.querySelector("#nhac2").addEventListener("click", function () {
+      document.querySelector("#audio source").src = audioSources[1];
+      document.querySelector("#audio").load(); 
+      document.querySelector("#audio").play();
+});
+
