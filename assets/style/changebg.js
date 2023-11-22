dd6b874bd64d22d02c172726c7cae12a045cabd2
Dương Thanh Sang <148996728+sangduong1507@users.noreply.github.com>
2023-11-23 00:45:52 +0700

    Update changebg.js

--- assets/style/changebg.js
+++ assets/style/changebg.js
@@ -1,29 +1,29 @@
 var bg = [
       './assets/images/bg1.png',
       './assets/images/bg2.jpg',
-  ];
-  
-  let currentBgIndex = 0;
-  
-  function changeBackground(index) {
+    ];
+    let currentBgIndex = 0;
+    
+    function changeBackground(index) {
       currentBgIndex = index;
+      console.log('Changing background to index:', index);
       document.getElementById('backgroundImage').src = bg[currentBgIndex];
-  }
-  
-  function toggleSettings() {
+    }
+    
+    function toggleSettings() {
       const backgroundOptions = document.getElementById('backgroundOptions');
       const displayStyle = getComputedStyle(backgroundOptions).display;
-  
-      if (displayStyle === "none") {
-          backgroundOptions.style.display = "block";
+      console.log('Toggle settings. Current display style:', displayStyle);
+      if (displayStyle === 'none') {
+        backgroundOptions.style.display = 'block';
       } else {
-          backgroundOptions.style.display = "none";
+        backgroundOptions.style.display = 'none';
       }
-  }
-  
-  function changeMusic(src) {
+    }
+    
+    function changeMusic(src) {
       const audioSource = document.getElementById('audioSource');
+      console.log('Changing music to:', src);
       audioSource.src = src;
       document.getElementById('audio').load();
-  }
-  
+    }
